a10ba01017c57a17fcc50f73ba9be2afbadd5680
josue rojas <josuerojas.rojas@gmail.com>
2018-11-13 17:27:11 -0500

    Updates

---
+++ precache-manifest.0b581724992bf6dd9ae86d20cd4e75ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c18977ca7dd705ac3b94",
+    "url": "/static/js/main.c18977ca.chunk.js"
+  },
+  {
+    "revision": "2ea0ccfca415dc7093bf",
+    "url": "/static/js/1.2ea0ccfc.chunk.js"
+  },
+  {
+    "revision": "c18977ca7dd705ac3b94",
+    "url": "/static/css/main.0634f5db.chunk.css"
+  },
+  {
+    "revision": "2ea0ccfca415dc7093bf",
+    "url": "/static/css/1.512a5127.chunk.css"
+  },
+  {
+    "revision": "143d28fcf5875974670111dfd8779cc9",
+    "url": "/index.html"
+  }
+];
